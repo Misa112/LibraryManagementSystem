37e2d60a82c655cc934ad48b3ba58b654d2f2e86
saugat10 <saugatkhatiwada10@gmail.com>
2022-05-12 11:10:42 +0200

    Added return book page

---
+++ LibraryManagementSystem/LibraryManagementSystem/Services/Interface/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Services.Interface
+{
+    public interface IUserService
+    {
+        IEnumerable<User> DisplayUsers();
+    }
+}
