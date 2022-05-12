bcaeacef15961f02ebb167b019f382fbcca1a4c2
saugat10 <saugatkhatiwada10@gmail.com>
2022-05-12 11:10:42 +0200

    Added return book page

---
+++ LibraryManagementSystem/LibraryManagementSystem/Services/EFServices/EFUserService.cs
@@ -0,0 +1,24 @@
+﻿using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Services.EFServices
+{
+    public class EFUserService : IUserService
+    {
+        private LibrarydbContext librarydbContext;
+
+        public EFUserService(LibrarydbContext context)
+        {
+            librarydbContext = context;
+        }
+
+        public IEnumerable<User> DisplayUsers()
+        {
+            return librarydbContext.Users.Where(a => a.IsAdmin == false);
+        }
+    }
+}
